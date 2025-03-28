be267e5a860f9a699c1966a5570d8de10e37c374
keevinz <kevin1080p.mdm@gmail.com>
2025-03-28 20:05:54 -0300

    Criado o AccountController.cs

---
+++ Store/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Tls;
+using Store.Models;
+using Store.ViewModels;
+
+
+namespace Store.Controllers;
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<Usuario> _signInManager;
+    private readonly UserManager<Usuario> _userManager;
+    private readonly IWebHostEnvironment _host;
+
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<Usuario> signInManager,
+        UserManager<Usuario> userManager,
+        IWebHostEnvironment host
+    )
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _host = host;
+    }
+
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM login = new()
+        {
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    } 
+}
